ceef22dc3dd52bb998443bea6acb30ca078fa1a8
tjebin <56176694+tjebin@users.noreply.github.com>
2023-12-24 19:45:43 -0500

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -5,9 +5,9 @@ export default () =>
     <>
         <h1>Home</h1>
         <div className="list-group">
-            <Link to="/furnitures/table" className="list-group-item">
-                Furniture Overview
-            </Link>
+            // <Link to="/furnitures/table" className="list-group-item">
+            //     Furniture Overview
+            // </Link>
             <Link to="/furnitures/grid" className="list-group-item">
                 Furniture Grid View
             </Link>
